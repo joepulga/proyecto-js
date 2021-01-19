9799fb0db516feeece3a69e39087c0bc88bdf82f
Joe Pulgarin <joe-pulga1@hotmail.com>
2021-01-19 18:10:57 -0500

    Se agrega el cambiador de temas, scroll automatico e identificaicon con el localStorage

--- js/main.js
+++ js/main.js
@@ -49,4 +49,51 @@ $(document).ready(()=>{
         $("#posts").append(post);
     });
 
+    // selector de tema
+    let theme = $("#theme");
+     
+    $("#to-green").click(function(){
+        theme.attr("href","css/green.css");
+        
+    });
+     $("#to-blue").click(function () {
+         theme.attr("href", "css/blue.css");
+    });
+    $("#to-red").click(function () {
+         theme.attr("href", "css/red.css");
+        
+    });
+   
+    // Scroll arriba de la web  
+    $('.up').click(function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop:0
+        },500);
+        return false
+    });
+
+    //Login falso
+
+    $("#login form").submit(function(){
+       let form_name =  $("#form_name").val();
+
+       localStorage.setItem("form_name",form_name);
+    }); 
+    let form_name = localStorage.getItem("form_name");
+
+    if(form_name != null && form_name != "undefined"){
+        let about_parrafo = $("#about p");
+
+        about_parrafo.html("<br><strong>Bienvenido," +form_name+ "</strong>");
+        about_parrafo.append(" <a href='#' id='logout'>Cerrar Sesión</a>")
+
+        $("#login").hide();
+
+        $("#logout").click(function(){
+            localStorage.clear();
+            location.reload();
+        });
+    }
+    
 });
